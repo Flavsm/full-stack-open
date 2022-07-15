86b96cd240cb639c6fae5afecdff2df9a7be3d09
Flavia Medeiros <flaardito@gmail.com>
2022-07-15 19:42:16 -0300

    finished unicafe

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,95 @@
+import { useState } from "react";
+
+const Button = ({ handleClick, text }) => {
+  return <button onClick={handleClick}>{text}</button>;
+};
+
+// const Stats = ({ total, keys, items }) => {
+//   if (total === 0) {
+//     return <div>No feddback given</div>;
+//   }
+//   return <div>{items}</div>;
+// };
+
+// const Items = ({ text, value }) => {
+//   return (
+//     <div>
+//       {text} {value}
+//     </div>
+//   );
+// };
+
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </tbody>
+  );
+};
+
+const Statistics = (props) => {
+  if (props.total === 0) {
+    return <div>No feddback given</div>;
+  }
+  return (
+    <div>
+      <table>
+        <StatisticLine text="good" value={props.good} />
+        <StatisticLine text="neutral" value={props.neutral} />
+        <StatisticLine text="bad" value={props.bad} />
+        <StatisticLine text="all" value={props.all} />
+        <StatisticLine text="average" value={props.average} />
+        <StatisticLine text="positive" value={props.positive} />
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+    setAverage(average + 1);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+    setAverage(average + 0);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+    setAverage(average - 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+
+      <h1>statistics</h1>
+      <Statistics
+        total={total}
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={total}
+        average={average / total}
+        positive={(good / total) * 100 + "%"}
+      />
+    </div>
+  );
+};
+
+export default App;
