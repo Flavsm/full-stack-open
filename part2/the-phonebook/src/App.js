8fae9a9fd5ce102a9b42f02e9c4948f3869b10b3
Flavia Medeiros <flaardito@gmail.com>
2022-07-25 17:29:27 -0300

    fetched db - the phonebook

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Numbers from "./components/Numbers";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newSearch, setNewSearch] = useState("");
   const [showAll, setShowAll] = useState(true);
+
+  const hook = () => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+    });
+  };
+
+  useEffect(hook, []);
 
   const handleInputChange = (event) => {
     event.target.id === "name"
