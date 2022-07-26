97b04436d4de32b5e8a885f4f132f80432d1e41a
Flavia Medeiros <flaardito@gmail.com>
2022-07-26 17:17:49 -0300

    working on "show" button

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -2,41 +2,94 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 const App = () => {
+  //state to define the db
   const [countries, setCountries] = useState([]);
+  //state to get input text
   const [search, setSearch] = useState("");
-  const [list, setList] = useState([]);
-  /* const [name, setName] = useState("");
-  const [capital, setCapital] = useState("");
-  const [area, setArea] = useState("");
-  const [lang, setLang] = useState("");
-  const [flag, setFlag] = useState(""); */
-  const [capital, setCapital] = useState("");
-  const [all, setAll] = useState([]);
+  //state to use button function
+  const [click, setClick] = useState(false);
 
+  //variable used to show data of one country
+  let info = [];
+
+  //variable fetching the data used with as Effect's hook
   const hook = () => {
     axios.get("https://restcountries.com/v3.1/all").then((res) => {
       setCountries(res.data);
     });
   };
 
+  //state updates after fetched data
   useEffect(hook, []);
 
+  //filter through the db to find the searched countries's name
   let data = countries
     .map((e) => e.name.common)
     .filter((e) => e.toLowerCase().startsWith(search.toLowerCase()));
 
+  //handles input change- gets target value and blocks button click feature
   const handleInputChange = (event) => {
     setSearch(event.target.value);
-    setList(data);
+    setClick(false);
   };
 
-  if (data.length === 1) {
-    let info = countries.filter(
-      (e) => e.name.common.toLowerCase() === data[0].toLowerCase()
+  //handle clicked button - filter the db with the clicked button
+  const handleClick = (event) => {
+    info = countries.filter(
+      (e) => e.name.common.toLowerCase() === event.target.value
     );
+    setClick(true);
+  };
 
-    info.map((e) =>
-      setAll([e.name.common, e.capital[0], e.area, e.languages, e.flags.png])
+  //filters the db to show countries searched and buttons
+  const filter = data.map((e) => {
+    return (
+      <div>
+        <li key={e}>
+          {e}{" "}
+          <button key={e + 1} value={e.toLowerCase()} onClick={handleClick}>
+            show
+          </button>
+        </li>
+      </div>
+    );
+  });
+
+  //button click action - display data or not
+  const button = info.map((e) => {
+    let obj = e.languages;
+    return (
+      <div>
+        <h1 key="name">{e.name.common}</h1>
+        <div key="div-1">
+          <p key="capital">capital: {e.capital}</p>
+          <p key="area">area: {e.area}</p>
+        </div>
+        <div>
+          <h3>languages:</h3>
+          <ul>
+            {Object.values(obj).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+        <img
+          key="flag"
+          src={e.flags.png}
+          alt="country flag"
+          border="1px solid black"
+        />
+      </div>
+    );
+  });
+
+  // conditional for button clicked or not - show filtered list or show country info of clicked button
+  let show = click ? button : filter;
+
+  //conditional to get data for one country searched
+  if (data.length === 1) {
+    info = countries.filter(
+      (e) => e.name.common.toLowerCase() === data[0].toLowerCase()
     );
   }
 
@@ -45,9 +98,43 @@ const App = () => {
       <form>
         find countries <input onChange={handleInputChange} type="text" />
       </form>
-      {list.length > 10
+
+      {data.length === 250 || data.length === 1
+        ? ""
+        : data.length > 10
         ? "Too many matches, specify another filter"
-        : list.map((e) => <li key={e}>{e}</li>)}
+        : show}
+
+      {info.map((e) => (
+        <h1 key="name">{e.name.common}</h1>
+      ))}
+      {info.map((e) => (
+        <div key="div-1">
+          <p key="capital">capital: {e.capital}</p>
+          <p key="area">area: {e.area}</p>
+        </div>
+      ))}
+      {info.map((e) => {
+        let obj = e.languages;
+        return (
+          <div>
+            <h3>languages:</h3>
+            <ul>
+              {Object.values(obj).map((value) => (
+                <li key={value}>{value}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+      {info.map((e) => (
+        <img
+          key="flag"
+          src={e.flags.png}
+          alt="country flag"
+          border="1px solid black"
+        />
+      ))}
     </div>
   );
 };
