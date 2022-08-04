924c649a8dec228c4bedaa103eb4bc341ac10239
Flavia Medeiros <flaardito@gmail.com>
2022-08-03 21:03:21 -0300

    finished step9

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import Form from "./components/Form";
 import Numbers from "./components/Numbers";
+import contactService from "./server/contacts";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
@@ -12,8 +12,8 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
-    axios.get("http://localhost:3001/persons").then((response) => {
-      setPersons(response.data);
+    contactService.getAll().then((initialContacts) => {
+      setPersons(initialContacts);
     });
   };
 
@@ -34,12 +34,49 @@ const App = () => {
       number: newNumber,
     };
 
-    persons.map((e) => e.name).includes(newName)
-      ? alert(`${newName} is already added to phonebook`)
-      : setPersons(persons.concat(nameObject));
+    /*  const changeNumber = (newName) => {
+      console.log(event.target);
+      let id = event.target.id;
+      if (
+        window.confirm(
+          `${newName} is already added to the phonebook, replace the old number with a new one?`
+        )
+      ) {
+        const note = persons.find((n) => n.id === id);
+        const changedContact = { ...note, number: newNumber };
+
+        contactService.update(id, changedContact).then((returnedContact) => {
+          setPersons(
+            persons.map((note) =>
+              note.id !== event.id ? note : changedContact
+            )
+          );
+        });
+      }
+    }; */
+
+    contactService.create(nameObject).then((addedContact) => {
+      persons.map((e) => e.name).includes(newName)
+        ? alert(`${newName} is already added to phonebook`) /* changeNumber(
+            newName
+          ) */
+        : setPersons(persons.concat(addedContact));
+    });
 
     setNewName("");
     setNewNumber("");
+  };
+
+  const handleDelete = (event) => {
+    let id = event.target.dataset.id;
+    let person = event.target.dataset.person;
+    if (window.confirm(`Delete ${person} ?`)) {
+      contactService.deleteOne(id).then((deleted) => {
+        contactService.getAll().then((initialContacts) => {
+          setPersons(initialContacts);
+        });
+      });
+    }
   };
 
   const listToShow = showAll
@@ -47,7 +84,6 @@ const App = () => {
     : persons.filter(
         (person) => person.name.toLowerCase() === newSearch.toLowerCase()
       );
-
   const handleFilter = () => {
     setShowAll(!showAll);
   };
@@ -68,8 +104,15 @@ const App = () => {
       />
       <Numbers
         display={listToShow.map((person) => (
-          <p key={person.name}>
+          <p key={person.id}>
             {person.name} {person.number}
+            <button
+              data-id={person.id}
+              data-person={person.name}
+              onClick={handleDelete}
+            >
+              delete
+            </button>
           </p>
         ))}
       />
